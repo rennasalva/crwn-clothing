2e07253fe93007af65a3efd06da3d85b77903075
renato salvatori <renato.s@cost.it>
2021-09-03 09:16:45 +0200

    Updates

---
+++ precache-manifest.1c4a9a502e33ac588bd97f84e02e2e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a3a5238788aa6875571",
+    "url": "/rennasalva/crwn-clothing.git/static/css/main.2396caf8.chunk.css"
+  },
+  {
+    "revision": "5a3a5238788aa6875571",
+    "url": "/rennasalva/crwn-clothing.git/static/js/main.52ba9286.chunk.js"
+  },
+  {
+    "revision": "ce487c6d776e7651a415",
+    "url": "/rennasalva/crwn-clothing.git/static/js/runtime~main.53c7ec05.js"
+  },
+  {
+    "revision": "fc9adf08fb1ce84d227c",
+    "url": "/rennasalva/crwn-clothing.git/static/js/2.7dada78d.chunk.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/rennasalva/crwn-clothing.git/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "c406e473f4ac01501dd6a90a7477133c",
+    "url": "/rennasalva/crwn-clothing.git/index.html"
+  }
+];
